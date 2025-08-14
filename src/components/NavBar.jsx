3d43989ae6800a72e96def9f1f9a0c6e87ebd845
chinnawat paksupho <chinnawatpaksupho@gmail.com>
2025-08-14 14:59:00 +0700

    edited NavLink

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,4 +1,5 @@
 import "../styles/navbar.module.css"
+import { NavLink } from "react-router";
 
 const NavBar = () => {
     const base = "/teach-react"
@@ -7,8 +8,8 @@ const NavBar = () => {
     <nav>
         <h3>React Hooks</h3>
         <ul>
-            <li><a href={`${base + "/use_state"}`}>useState</a></li>
-            <li><a href={`${base + "/use_effect"}`}>useEffect</a></li>
+            <li><NavLink to={`${base + "/use_state"}`}>useState</NavLink></li>
+            <li><NavLink to={`${base + "/use_effect"}`}>useEffect</NavLink></li>
             <li>useMemo</li>
             <li>useCallback</li>
         </ul>
